0a78a5203ccfb12e9b1675b8aacd238a4c21cf4a
Sk Irfan Ahmed <skirfanahmed2206@gmail.com>
2026-08-11 22:25:14 +0530

    First Commit

---
+++ Codes/shell.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    while (true) {
+        string text;
+        cout << "basic > ";
+        getline(cin, text);
+        cout << text << endl;
+    }
+    return 0;
+}
